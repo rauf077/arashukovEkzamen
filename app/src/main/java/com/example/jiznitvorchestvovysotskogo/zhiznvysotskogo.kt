686d4f0909342f9de8028973dfa16580e89945f5
Your Name <you@example.com>
2022-12-01 17:43:19 +0300

    first commit

---
+++ app/src/main/java/com/example/jiznitvorchestvovysotskogo/zhiznvysotskogo.kt
@@ -0,0 +1,11 @@
+package com.example.jiznitvorchestvovysotskogo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class zhiznvysotskogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zhiznvysotskogo)
+    }
+}
